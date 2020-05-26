b6b4894a6e445e14d0fd1b1d846c4b3392fc80c0
Akash Acharya <akshachrya@gmail.com>
2020-05-26 22:57:34 +0545

    1. add end date 2. make table between start date and end date

--- static/js/home.js
+++ static/js/home.js
@@ -2,24 +2,37 @@ $('#cityform').on('submit',function(e){
 	e.preventDefault();
 	city=$('#cityselect').val()
 	date=$('#citydate').val()
-	data={'city':city,'date':date}
-
+	date1=$('#citydate1').val()
+	data={'city':city,'date':date,'date1':date1}
+	$('.loader').show();
 	$.ajax({
 	 url:'/send',
 	 type:'POST',
 	 contentType: 'application/json',
 	 data:JSON.stringify(data),
 	 success:function(response){
+	 $('#hidetable').hide();
 	  var res=JSON.parse(response);
 	  var table1=res['table1']
 	  var table2=res['table2']
-	  console.log(table2)
-	  $('#cityname').html(table1['city'])
+	   $('#cityname').html(table1['city'])
 	  $('#cityregion').html(table1['region'])
 	  $('#timezone').html(table1['timezone'])
 		$('#longitude').html(table1['long'])
 		$('#latitude').html(table1['lat'])
-		$('#tablesss').html('<td>'+table2['date']+'</td><td>'+table2['dawn']+'<td>'+table2['sunrise']+'</td><td>'+table2['noon']+'</td><td>'+table2['sunset']+'</td><td>'+table2['dusk']+'</td>')
+
+
+	  var tabledata=''
+	  tabledata+='<table style="width:100%" class="table table-condensed">'
+	   tabledata+='<tr><th>Date</th><th>Dawn</th><th>Sunrise</th><th>Noon</th><th>Sunset</th><th>Dusk</th></tr>'
+           
+	 
+		for(var i=0;i<table2.length;i++){
+			tabledata+='<tr><td>'+table2[i]['date']+'</td><td>'+table2[i]['dawn']+'<td>'+table2[i]['sunrise']+'</td><td>'+table2[i]['noon']+'</td><td>'+table2[i]['sunset']+'</td><td>'+table2[i]['dusk']+'</td></tr>'
+		}
+		tabledata+='</table>'
+
+		$('#tablesss').html(tabledata)
 
 
 	 }
--- views/api.py
+++ views/api.py
@@ -1,8 +1,8 @@
 from flask import Flask,render_template,request,Blueprint
 from astral import LocationInfo
-from datetime import datetime
 from astral.sun import sun
 from astral.geocoder import database, lookup
+from datetime import datetime,timedelta
 import json
 import pytz
 
@@ -12,16 +12,20 @@ api=Blueprint('api',__name__)
 def send():
 	if request.method=='POST':
 		data=request.json
-		orgdate=data['date']
+		startdate=datetime.strptime(data['date'],'%Y-%m-%d')
+		enddate=datetime.strptime(data['date1'],'%Y-%m-%d')
 		cityinfo=data['city']
 		city=lookup(cityinfo, database())
-		date=datetime.strptime(orgdate, "%Y-%m-%d")
-		suns=sun(city.observer, date=date, tzinfo=pytz.timezone(city.timezone))
-		dawn=suns['dawn'].strftime("%H:%M:%S")
-		sunrise=suns['sunrise'].strftime("%H:%M:%S")
-		sunset=suns['sunset'].strftime("%H:%M:%S")
-		dusk=suns['dusk'].strftime("%H:%M:%S")
-		noon=suns['noon'].strftime("%H:%M:%S")
+		day = timedelta(days=1)
+		table2=[]
+		while startdate <= enddate:
+			suns=sun(city.observer, date=startdate, tzinfo=pytz.timezone(city.timezone))
+			dawn=suns['dawn'].strftime("%H:%M:%S")
+			sunrise=suns['sunrise'].strftime("%H:%M:%S")
+			sunset=suns['sunset'].strftime("%H:%M:%S")
+			dusk=suns['dusk'].strftime("%H:%M:%S")
+			noon=suns['noon'].strftime("%H:%M:%S")
+			table2.append({'date':startdate.strftime('%Y-%m-%d'),'dawn':dawn,'sunrise':sunrise,'sunset':sunset,'dusk':dusk,'noon':noon})
+			startdate=startdate+day
 		table1={'city':city.name,'region':city.region,'timezone':city.timezone,'long':round(city.longitude,2),'lat':round(city.latitude,2)}
-		table2={'date':orgdate,'dawn':dawn,'sunrise':sunrise,'sunset':sunset,'dusk':dusk,'noon':noon}
 		return json.dumps({'table1':table1,'table2':table2})
